d457741d0d08903f2dec9b6c344f5888e1f3eccf
Gregory fournier <gregory.fournier@protonmail.com>
2020-12-02 08:23:25 -0500

    Day 2 complete.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,44 @@
 import java.io.File
 
 fun main(args: Array<String>) {
-   day1()
+    day2part2()
 }
+
+fun day2part2() {
+    val lines = readFile("2.txt")
+    var validPasswords = 0
+    lines.forEach { line ->
+        val attributes = line.split(" ")
+        val values = attributes[0].split("-")
+        val firstPosition = values[0].toInt() - 1
+        val secondPosition = values[1].toInt() - 1
+        val letter = attributes[1][0]
+        val password = attributes[2]
+        if ((password[firstPosition] == letter).xor(password[secondPosition] == letter)) {
+            validPasswords++
+        }
+    }
+    println(validPasswords)
+}
+
+fun day2() {
+    val lines = readFile("2.txt")
+    var validPasswords = 0
+    lines.forEach { line ->
+        val attributes = line.split(" ")
+        val values = attributes[0].split("-")
+        val min = values[0].toInt()
+        val max = values[1].toInt()
+        val letter = attributes[1][0]
+        val password = attributes[2]
+        val actualCount = password.split(letter).size - 1
+        if (actualCount in min..max) {
+            validPasswords++
+        }
+    }
+    println(validPasswords)
+}
+
 
 fun day1() {
     // For part one simply remove the third loop in getBothNumbers
@@ -11,7 +47,7 @@ fun day1() {
     println(pair.first * pair.second * pair.third)
 }
 
-private fun getBothNumbers(numbers: List<Int>): Triple<Int,Int,Int> {
+private fun getBothNumbers(numbers: List<Int>): Triple<Int, Int, Int> {
     numbers.forEachIndexed { index, number ->
         numbers.subList(index + 1, numbers.lastIndex).forEach { secondNumber ->
             numbers.subList(index + 2, numbers.lastIndex).forEach { thirdNumber ->
